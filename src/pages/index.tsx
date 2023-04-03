3f10a2cd6808c57921509d389ed3df7efe0ea9cd
koushith <koushith97@gmail.com>
2023-04-04 01:48:10 +0530

    adds basic structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,5 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <h1 style={{ fontSize: "1rem" }}>Hello</h1>
-    </>
-  );
+  return <h2>Home</h2>;
 }
