827c3f391f1f8016907c7aead89d1e6fc64a63fe
koushith <koushith97@gmail.com>
2023-07-19 12:51:40 +0530

    removes nextjs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true, //fixes ssr class name missmatch
-  },
-};
-
-module.exports = nextConfig;
