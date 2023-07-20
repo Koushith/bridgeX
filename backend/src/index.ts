342f1fc5fac4580e92c219bb144421d8ac519546
koushith <koushith97@gmail.com>
2023-07-21 03:23:10 +0530

    adds express middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import dotenv from "dotenv";
 
+import { errorHandler, notFound } from "./middlewares/errorHandler.js";
 import { connectToDB } from "./utils/db.js";
 
 dotenv.config();
@@ -13,9 +14,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("This route works");
 });
+
+//custom middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port - ${PORT}`);
