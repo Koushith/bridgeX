0de1cd846eb64982519b0455b8da61427fe8e750
koushith <koushith97@gmail.com>
2023-10-25 15:24:53 +0530

    adds readme

--- backend/src/controllers/user/user.controller.ts
+++ backend/src/controllers/user/user.controller.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express'
 
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
-import { asyncHandler } from "../../middlewares/asyncHandler.js";
-import { User } from "../../models/user.model.js";
+import { asyncHandler } from '../../middlewares/asyncHandler.js'
+import { User } from '../../models/user.model.js'
 
 /**
  *  @description - authorize the user and send token
  */
 export const authUser = asyncHandler(async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
 
   //this will check if the hashed password is as same as passoword stored in the database
   //@ts-ignore TODO: check for type safy later
@@ -20,62 +20,62 @@ export const authUser = asyncHandler(async (req: Request, res: Response) => {
       {
         userId: user?._id,
       },
-      "5f1nc1ujbc", // TODO: move this to env- change the key while deploying
+      '5f1nc1ujbc', // TODO: move this to env- change the key while deploying
       {
-        expiresIn: "30d",
+        expiresIn: '30d',
       }
-    );
+    )
     // setting this on cookie, we are not sending this token in response. much safer and prevents cross side attacks!!
-    res.cookie("jwt", token, {
+    res.cookie('jwt', token, {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 1000, //30days
-      secure: process.env.NODE_ENV !== "development",
-    });
+      secure: process.env.NODE_ENV !== 'development',
+    })
 
     res.status(200).json({
       user,
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
 /**
  * @desc    Get all users
  */
 export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
-  console.log("get all users working.");
+  console.log('get all users working.')
 
-  const query = await User.find({});
+  const query = await User.find({})
 
   if (!query) {
-    throw new Error("No user found");
+    throw new Error('No user found')
   } else {
     res.status(200).json({
       success: true,
       data: query,
-    });
+    })
   }
-});
+})
 
 /**
  * @desc    Get user by id
  */
 
 export const getUserById = asyncHandler(async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const user = await User.findById({ id });
+  const id = req.params.id
+  const user = await User.findById({ id })
   if (!user) {
-    res.status(404);
-    throw new Error("User not found");
-    return;
+    res.status(404)
+    throw new Error('User not found')
+    return
   }
 
   res.status(200).json({
     success: true,
     data: user,
-  });
-});
+  })
+})
 
 //delete user - admin only
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,7 +1,8 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs'
 
+//TODO: Refactor this. add few more meta details
 const walletSchema = new mongoose.Schema({
   walletName: {
     type: String,
@@ -9,7 +10,7 @@ const walletSchema = new mongoose.Schema({
   address: {
     type: String,
   },
-});
+})
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,10 +24,10 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
   wallets: [walletSchema],
-});
+})
 
 userSchema.methods.comparePassword = async (enteredPassword: string) => {
-  return await bcrypt.compare(enteredPassword, this?.password);
-};
+  return await bcrypt.compare(enteredPassword, this?.password)
+}
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model('User', userSchema)
